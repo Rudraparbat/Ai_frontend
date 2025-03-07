ca9ef721209f969d3046d8c35184605e1515630c
anasuya100 <royanasuya346@gmail.com>
2025-03-08 01:19:55 +0530

    updated api url

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
     setIsLoading(true);
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/get", {
+      const response = await fetch("https://aiapi-vdez.onrender.com//api/get", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
